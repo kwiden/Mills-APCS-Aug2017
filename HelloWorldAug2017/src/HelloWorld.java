c265cb38156b5897a577554927aa9c0ff77ee692
Steven Lamont <Steve@Lamont.email>
2017-08-29 09:26:21 -0700

    in class update

--- HelloWorldAug2017/src/HelloWorld.java
+++ HelloWorldAug2017/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World from Steve");
 		System.out.println("hello again");
-		System.out.println("hello again");
+		System.out.println("hello hello");
 
 	}
 
