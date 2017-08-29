138593cf1e6b5226ca2435e29c1da2eecd786aff
Steven Lamont <Steve@Lamont.email>
2017-08-29 08:15:56 -0700

    trying it new way

--- HelloWorldAug2017/src/HelloWorld.java
+++ HelloWorldAug2017/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World from Steve");
 		System.out.println("hello again");
+		System.out.println("hello again");
 
 	}
 
